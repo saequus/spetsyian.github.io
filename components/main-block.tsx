8c25595c6034e8001551ea1abe04f40a3e2fbcb4
saequus <slava@spetsyian.com>
2021-10-19 17:11:50 +0200

    [Feat] game of life

--- components/main-block.tsx
+++ components/main-block.tsx
@@ -24,6 +24,7 @@ const MainBlock = ({
       </div>
 
       <div className='m-auto'>
+
         <div className='flex flex-col text-center tracking-wider'>
           <h1 className='text-3xl uppercase font-semibold m-2 orange shadow-t-bl'>Slava Spetsyian</h1>
           <p className='text-center text-xl uppercase font-extralight gray-600 m-1'>Full Stack Engineer. Growth Hacker. Author</p>
@@ -37,7 +38,6 @@ const MainBlock = ({
                 <a className='orange' href='https://t.me/slava_spetsyian'>@slava_spetsyian</a>
               </div>
             </div>
- 
         </div>
 
         <div className='flex justify-center'>
@@ -48,7 +48,7 @@ const MainBlock = ({
             <Button href='/en/about/' text='More'></Button>
           </div>
 
-          <div className='p-8 m-7 flex-auto w-2/12 rounded-sm chocolate-bg orange shadow-xl'>
+          <div className='p-8 m-7 flex-auto w-2/12 rounded-sm chocolate-bg orange shadow-2xl'>
             <h1 className='text-3xl p-1 font-semibold'>CV</h1>
             <p className='mb-2 uppercase font-extralight'>See the CV / Resume</p>
             <Button href='/assets/Slava_Spetsyian_CV.pdf' text='See'></Button>
@@ -59,12 +59,12 @@ const MainBlock = ({
         <div className='flex justify-center'>
 
           <div className='p-8 m-7 flex-auto w-2/12 rounded-sm chocolate-bg orange uppercase shadow-2xl'>
-            <h1 className='text-3xl p-1 tont-semibold'>Blog</h1>
+            <h1 className='text-3xl p-1 font-semibold'>Blog</h1>
             <p className='mb-2 font-extralight'>Blog in English and Russian</p>
             <Button href='/en/blog/' text='Visit'></Button>
           </div>
 
-          <div className='p-8 m-7 flex-auto w-2/12 rounded-sm chocolate-bg orange shadow-2xl'>
+          <div className='p-8 m-7 flex-auto w-2/12 rounded-sm chocolate-bg orange uppercase shadow-2xl'>
             <h1 className='text-3xl p-1 font-semibold'>Shared</h1>
             <p className='mb-2 font-extralight'>Staff you may find funny or usefull</p>
             <Button href='https://drive.google.com/drive/u/0/folders/1AzDEjiloqka5BDha16IxJaBQVBU2CYej' text='Check'></Button>
