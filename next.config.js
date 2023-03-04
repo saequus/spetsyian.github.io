012f72ddc768f305836ffda77b95ee38f05c2827
saequus <slava@spetsyian.com>
2023-03-04 22:53:01 +0100

    [Feat] serve game of life inside next js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   images: {
     formats: ['image/avif', 'image/webp']
-  }
+  },
+  rewrites: async () => [
+    {
+      source: "/apps/game-of-life",
+      destination: "/assets/apps/game-of-life.html",
+    }
+  ],
 }
